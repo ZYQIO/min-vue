428c5c94103c6612caea21d23001460ea8ea5caf
ZYQIO <13477681517@163.com>
2022-09-30 20:39:15 +0800

    feat: 实现 effect 的 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,7 +1,11 @@
+import { extend } from "../../shared";
 
 class ReactiveEffect {
     private _fn: any
     scheduler: any;
+    deps = []
+    active = true
+    onStop?: () => void
 
     constructor(fn, scheduler) {
         this._fn = fn
@@ -11,6 +15,21 @@ class ReactiveEffect {
         activeEffect = this
         return this._fn()
     }
+    stop() {
+        if (this.active) {
+            cleanupEffect(this)
+            if (this.onStop) {
+                this.onStop()
+            }
+            this.active = false
+        }
+    }
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
 }
 
 const targetMap = new Map()
@@ -33,7 +52,10 @@ export function track(target, key) {
         depsMap.set(key, dep)
     }
 
+    if (!activeEffect) return;
+
     dep.add(activeEffect)
+    activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -52,8 +74,19 @@ let activeEffect;
 export function effect(fn, options: any = {}) {
     const _effect = new ReactiveEffect(fn, options.scheduler)
 
+    // _effect.onStop = options.onStop;
+    // Object.assign(_effect, options)
+    // extend
+    extend(_effect, options)
+
     _effect.run()
 
-    return _effect.run.bind(_effect)
+    const runner: any = _effect.run.bind(_effect)
+    runner._effect = _effect
+
+    return runner;
 }
 
+export function stop(runner) {
+    runner._effect.stop()
+}
