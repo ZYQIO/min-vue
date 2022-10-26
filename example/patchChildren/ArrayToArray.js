456e40017ad54de54970ed492201577a5209f2ca
ZYQIO <13477681517@163.com>
2022-10-26 15:32:57 +0800

    feat: 更新 element 的 children - 双端对比diff算法 (2)

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -82,16 +82,16 @@ import { ref, h } from '../../lib/guide-mini-vue.esm.js'
 // ];
 // const nextChildren = [h("p", { key: "B" }, "B"), h("p", { key: "C" }, "C")];
 
-const prevChildren = [
-    h("p", { key: "A" }, "A"),
-    h("p", { key: "B" }, "B"),
-];
-const nextChildren = [
-    h("p", { key: "D" }, "D"),
-    h("p", { key: "C" }, "C"),
-    h("p", { key: "A" }, "A"),
-    h("p", { key: "B" }, "B"),
-];
+// const prevChildren = [
+//     h("p", { key: "A" }, "A"),
+//     h("p", { key: "B" }, "B"),
+// ];
+// const nextChildren = [
+//     h("p", { key: "D" }, "D"),
+//     h("p", { key: "C" }, "C"),
+//     h("p", { key: "A" }, "A"),
+//     h("p", { key: "B" }, "B"),
+// ];
 
 
 // 5. 对比中间的部分
@@ -102,23 +102,23 @@ const nextChildren = [
 // D 节点在新的里面是没有的 - 需要删除掉
 // C 节点 props 也发生了变化
 
-// const prevChildren = [
-//   h("p", { key: "A" }, "A"),
-//   h("p", { key: "B" }, "B"),
-//   h("p", { key: "C", id: "c-prev" }, "C"),
-//   h("p", { key: "D" }, "D"),
-//   h("p", { key: "F" }, "F"),
-//   h("p", { key: "G" }, "G"),
-// ];
+const prevChildren = [
+    h("p", { key: "A" }, "A"),
+    h("p", { key: "B" }, "B"),
+    h("p", { key: "C", id: "c-prev" }, "C"),
+    h("p", { key: "D" }, "D"),
+    h("p", { key: "F" }, "F"),
+    h("p", { key: "G" }, "G"),
+];
 
-// const nextChildren = [
-//   h("p", { key: "A" }, "A"),
-//   h("p", { key: "B" }, "B"),
-//   h("p", { key: "E" }, "E"),
-//   h("p", { key: "C", id:"c-next" }, "C"),
-//   h("p", { key: "F" }, "F"),
-//   h("p", { key: "G" }, "G"),
-// ];
+const nextChildren = [
+    h("p", { key: "A" }, "A"),
+    h("p", { key: "B" }, "B"),
+    h("p", { key: "E" }, "E"),
+    h("p", { key: "C", id: "c-next" }, "C"),
+    h("p", { key: "F" }, "F"),
+    h("p", { key: "G" }, "G"),
+];
 
 // 5.1.1
 // a,b,(c,e,d),f,g
